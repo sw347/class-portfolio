4d1af6be234f27c8c2086ce905f74c957c23dea5
sw347 <>
2025-04-25 10:48:18 +0900

    server port 변경2

--- public/board-script.js
+++ public/board-script.js
@@ -14,7 +14,7 @@ const highlighted = document.querySelector(".highlighted");
 
 const searchItem = document.querySelector(".search-item");
 
-const host = "https://https://european-chem-studies-vitamins.trycloudflare.com";
+const host = "https://european-chem-studies-vitamins.trycloudflare.com";
 
 const namedItem = (name) => {
   let named = document.createElement("div");
