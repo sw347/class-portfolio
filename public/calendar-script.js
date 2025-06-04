7b5ca169b0bb53c5b7bcb277682d139704150c8d
sw347 <>
2025-06-04 20:01:57 +0900

    달력에 공휴일과 기념일을 추가

--- public/calendar-script.js
+++ public/calendar-script.js
@@ -1,10 +1,102 @@
-function generateCalendar(date, set = 0) {
+let url = null;
+const serviceKey = decodeURI(
+  "g2kzCadBgw+Z9bZGwt1tXs4RbTpTRIVoGhSAx9loGRIyCEH65Q0ashBsvunxPSUl7EqIEeu0qyIgTpS3etZLSQ=="
+);
+
+let year = null;
+let month = null;
+const holidays = [];
+const holidaysCache = {};
+const anniversarys = [];
+const anniversarysCache = {};
+
+async function checkholidays() {
+  if (holidaysCache[year]) {
+    // 이미 캐시에 있으면 복사해서 사용
+    holidays.length = 0;
+    holidays.push(...holidaysCache[year]);
+    return;
+  }
+
+  url =
+    "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo";
+
+  const queryParams = new URLSearchParams({
+    serviceKey: serviceKey,
+    solYear: year,
+  });
+
+  await fetch(`${url}?${queryParams.toString()}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res.text();
+    })
+    .then((data) => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, "text/xml");
+
+      const items = xmlDoc.getElementsByTagName("item");
+
+      for (let i = 0; i < items.length; i++) {
+        const locdate = items[i].getElementsByTagName("locdate")[0].textContent;
+        holidays.push(locdate);
+      }
+    })
+    .catch((error) => {
+      console.error("에러 발생:", error);
+      alert("에러 발생: 콘솔을 확인하세요.");
+    });
+}
+
+async function checkAnniversary() {
+  if (anniversarysCache[year]) {
+    // 이미 캐시에 있으면 복사해서 사용
+    anniversarys.length = 0;
+    anniversarys.push(...anniversarysCache[year]);
+    return;
+  }
+
+  url =
+    "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getAnniversaryInfo";
+
+  const queryParams = new URLSearchParams({
+    serviceKey: serviceKey,
+    solYear: year,
+  });
+
+  await fetch(`${url}?${queryParams.toString()}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res.text();
+    })
+    .then((data) => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, "text/xml");
+
+      const items = xmlDoc.getElementsByTagName("item");
+
+      for (let i = 0; i < items.length; i++) {
+        const locdate = items[i].getElementsByTagName("locdate")[0].textContent;
+        anniversarys.push(locdate);
+      }
+    })
+    .catch((error) => {
+      console.error("에러 발생:", error);
+      alert("에러 발생: 콘솔을 확인하세요.");
+    });
+}
+
+async function generateCalendar(date, set = 0) {
   let calendarContainer = document.getElementById("calendar");
 
   let currentDate = date;
 
-  let year = currentDate.getFullYear();
-  let month = currentDate.getMonth() + set;
+  year = currentDate.getFullYear();
+  month = currentDate.getMonth() + set;
 
   if (month > 11) {
     year += Math.floor(month / 12);
@@ -13,6 +105,10 @@ function generateCalendar(date, set = 0) {
     year -= Math.ceil(Math.abs(month) / 12);
     month = 11 + ((month + 1) % 12);
   }
+
+  holidays.length = 0;
+  await checkholidays();
+  await checkAnniversary();
 
   var calendarHTML = `<p>${year}년 ${month + 1}월</p>`;
 
@@ -39,7 +135,15 @@ function generateCalendar(date, set = 0) {
       } else if (dayCounter > totalDaysInMonth) {
         calendarHTML += "<td></td>";
       } else {
+        const dateStr =
+          `${year}` +
+          `${month + 1}`.padStart(2, "0") +
+          `${dayCounter}`.padStart(2, "0");
+
         let style = "";
+        if (holidays.includes(dateStr)) style += "color: green;"; // 공휴일
+        if (anniversarys.includes(dateStr)) style += "color: violet";
+
         if (j === 0) style = "color: red;"; // 일요일 (빨간색)
         if (j === 6) style = "color: blue;"; // 토요일 (파란색)
 
@@ -62,17 +166,11 @@ function generateCalendar(date, set = 0) {
   calendarHTML += "</table>";
 
   calendarContainer.innerHTML = calendarHTML;
+
+  marker();
 }
 
-// 페이지 로드 시 달력 생성
-window.onload = function () {
-  generateCalendar(new Date());
-
-  let count = 0;
-
-  const beforeMonth = document.querySelector(".before-month");
-  const nextMonth = document.querySelector(".next-month");
-
+function marker() {
   const tdList = document.querySelectorAll("td");
 
   tdList.forEach((item) => {
@@ -91,6 +189,16 @@ window.onload = function () {
       if (item.textContent != "") item.classList.toggle("toggle");
     });
   });
+}
+
+// 페이지 로드 시 달력 생성
+window.onload = async function () {
+  await generateCalendar(new Date());
+
+  let count = 0;
+
+  const beforeMonth = document.querySelector(".before-month");
+  const nextMonth = document.querySelector(".next-month");
 
   nextMonth.addEventListener("click", () => {
     generateCalendar(new Date(), ++count);
